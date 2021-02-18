5d5dde385ae8e6c75c1b394f4114986e3b1f3ca4
devansh-dvj <devansh.dvj@gmail.com>
2021-02-18 19:47:53 +0530

    Q2: Added base of FourbitComp

---
+++ Q2/FourbitComp.vhd
@@ -0,0 +1,23 @@
+-- TOP MODULE: FourbitComp
+library work;
+use work.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity FourbitComp is
+   port (a, b : in std_logic_vector (1 downto 0);
+         l, g, e : out std_logic);
+end entity;
+
+architecture Comparator of FourbitComp is
+
+   component TwoByOneMux is
+      port (i : in std_logic_vector(1 downto 0);
+            sel : in std_logic;
+            z : out std_logic);
+   end component;
+
+begin
+
+end architecture;
