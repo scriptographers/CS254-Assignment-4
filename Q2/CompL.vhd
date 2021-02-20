a77616587649668c14a20948fe1e04a841d7770b
Harshit Varma <varmaharshit2@gmail.com>
2021-02-20 16:56:27 +0530

    Added Q2: g and l

--- Q2/CompL.vhd
+++ Q2/CompL.vhd
@@ -12,7 +12,7 @@ end entity;
 
 architecture l_arc of CompL is
 
-   signal sig: std_logic_vector(4 downto 0);
+   signal b1_1, b0_1, b0_2, a1_1, a1_2: std_logic;
 
    component TwoByOneMux is
       port (i : in std_logic_vector(1 downto 0);
@@ -22,17 +22,22 @@ architecture l_arc of CompL is
 
 begin
 
-   mux_b1_0: TwoByOneMux
-      port map (i(0) => '0', i(1) => '1', sel => b(1), z => sig(0));
    mux_b1_1: TwoByOneMux
-      port map (i(0) => '1', i(1) => '0', sel => b(1), z => sig(1));
-   mux_a1: TwoByOneMux
-      port map (i(0) => sig(1), i(1) => sig(0), sel => a(1), z => sig(2));
-   mux_b0_0: TwoByOneMux
-      port map (i(0) => '0', i(1) => sig(2), sel => b(0), z => sig(3));
-   mux_b0_1: TwoByOneMux
-      port map (i(0) => sig(2), i(1) => '0', sel => b(0), z => sig(4));
-   mux_a0: TwoByOneMux
-      port map (i(0) => sig(4), i(1) => sig(3), sel => a(0), z => z);
+      port map (i(0) => '0', i(1) => '1', sel => b(1), z => b1_1);
+	
+	mux_b0_1: TwoByOneMux
+      port map (i(0) => '0', i(1) => b1_1, sel => b(0), z => b0_1);
+		
+	mux_b0_2: TwoByOneMux
+      port map (i(0) => b1_1, i(1) => '1', sel => b(0), z => b0_2);
+   
+	mux_a1_1: TwoByOneMux
+      port map (i(0) => '1', i(1) => b1_1, sel => a(1), z => a1_1);
+		
+	mux_a1_2: TwoByOneMux
+      port map (i(0) => b0_2, i(1) => b0_1, sel => a(1), z => a1_2);
+	
+	mux_a0_1: TwoByOneMux
+      port map (i(0) => a1_1, i(1) => a1_2, sel => a(0), z => z);
 
 end architecture;
