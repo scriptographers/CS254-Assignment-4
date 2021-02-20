3486c897b37fbc0b1c8649dd5e45ec327c8eaf41
Harshit Varma <varmaharshit2@gmail.com>
2021-02-20 17:18:42 +0530

    Q2: Fixed bug, tested, added waveform

--- Q2/CompL.vhd
+++ Q2/CompL.vhd
@@ -23,21 +23,21 @@ architecture l_arc of CompL is
 begin
 
    mux_b1_1: TwoByOneMux
-      port map (i(0) => '0', i(1) => '1', sel => b(1), z => b1_1);
+      port map (i(1) => '1', i(0) => '0', sel => b(1), z => b1_1);
 	
 	mux_b0_1: TwoByOneMux
-      port map (i(0) => '0', i(1) => b1_1, sel => b(0), z => b0_1);
+      port map (i(1) => '1', i(0) => b1_1, sel => b(0), z => b0_1);
 		
 	mux_b0_2: TwoByOneMux
-      port map (i(0) => b1_1, i(1) => '1', sel => b(0), z => b0_2);
+      port map (i(1) => b1_1, i(0) => '0', sel => b(0), z => b0_2);
    
 	mux_a1_1: TwoByOneMux
-      port map (i(0) => '1', i(1) => b1_1, sel => a(1), z => a1_1);
+      port map (i(1) => '0', i(0) => b1_1, sel => a(1), z => a1_1);
 		
 	mux_a1_2: TwoByOneMux
-      port map (i(0) => b0_2, i(1) => b0_1, sel => a(1), z => a1_2);
+      port map (i(1) => b0_2, i(0) => b0_1, sel => a(1), z => a1_2);
 	
 	mux_a0_1: TwoByOneMux
-      port map (i(0) => a1_1, i(1) => a1_2, sel => a(0), z => z);
+      port map (i(1) => a1_1, i(0) => a1_2, sel => a(0), z => z);
 
 end architecture;
